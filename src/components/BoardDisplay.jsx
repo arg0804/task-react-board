81533faca300a993bbd5eccc401d292dc2f77a2d
Argishti <argishthakobyan777@gmail.com>
2023-10-26 17:04:28 +0400

    first commit

--- src/components/BoardDisplay.jsx
+++ src/components/BoardDisplay.jsx
@@ -2,66 +2,120 @@ import React, { useState, useEffect } from 'react';
 import classNames from 'classnames';
 import './BoardDisplay.css';
 
+
+function binPacking(cuts, binWidth, binHeight) {
+  const sortedCuts = [...cuts].sort((a, b) => b.height - a.height);
+
+  const bins = [{ width: binWidth, height: binHeight, blocks: [] }];
+  const smallBlocks = [];
+
+  sortedCuts.forEach((cut) => {
+    for (let i = 0; i < cut.quantity; i++) {
+      let bestFitBin = null;
+
+      if (cut.width > binWidth || cut.height > binHeight) {
+        smallBlocks.push(cut);
+        continue;
+      }
+
+      for (const bin of bins) {
+        if (cut.width <= bin.width && cut.height <= bin.height) {
+          if (!bestFitBin || (bin.width - cut.width) * (bin.height - cut.height) <
+              (bestFitBin.width - cut.width) * (bestFitBin.height - cut.height)) {
+            bestFitBin = bin;
+          }
+        }
+      }
+
+      if (bestFitBin) {
+        const newBlock = { width: cut.width, height: cut.height };
+        bestFitBin.blocks.push(newBlock);
+
+        bestFitBin.width -= cut.width;
+        bestFitBin.height -= cut.height;
+      } else {
+        if (cut.width <= binWidth && cut.height <= binHeight) {
+          for (const bin of bins) {
+            if (bin.width >= cut.width && bin.height >= cut.height) {
+              const newBlock = { width: cut.width, height: cut.height };
+              bin.blocks.push(newBlock);
+
+              bin.width -= cut.width;
+              bin.height -= cut.height;
+              break;
+            }
+          }
+        }
+
+        if (!bestFitBin) {
+          // If there's no bin that can fit the cut, create a new bin
+          const newBin = { width: binWidth, height: binHeight, blocks: [] };
+          newBin.blocks.push({ width: cut.width, height: cut.height });
+          bins.push(newBin);
+        }
+      }
+    }
+  });
+
+  // Add small blocks to fill empty spaces
+  smallBlocks.forEach((cut) => {
+    for (const bin of bins) {
+      if (cut.width <= bin.width && cut.height <= bin.height) {
+        const newBlock = { width: cut.width, height: cut.height };
+        bin.blocks.push(newBlock);
+
+        bin.width -= cut.width;
+        bin.height -= cut.height;
+        break;
+      }
+    }
+  });
+
+  return bins;
+}
+
+
+
+
+
+
+
+
 function BoardDisplay({ cuts, boardWidth, boardHeight, selectedImage }) {
   const [blocks, setBlocks] = useState([]);
 
-useEffect(() => {
-let totalBlockHeight = 0;
-  let leftPosition = 0;
-  const newBlocks = [];
-  for (const cut of cuts) {
-    for (let i = 0; i < cut.quantity; i++) {
-      if (cut.height <= boardHeight && cut.width <= boardWidth - leftPosition) {
-        const block = {
-          width: cut.width,
-          height: cut.height,
-          left: leftPosition,
-          top: totalBlockHeight,
-        };
-        leftPosition += cut.width;
-        newBlocks.push(block);
-      } else {
-        leftPosition = 0;
-        totalBlockHeight += cut.height;
-        const block = {
-          width: cut.width,
-          height: cut.height,
-          left: leftPosition,
-          top: totalBlockHeight,
-        };
-        leftPosition += cut.width;
-        newBlocks.push(block);
-      }
-    }
-  }
-  setBlocks(newBlocks);
-}, [cuts, boardWidth, boardHeight]);
+  useEffect(() => {
+    const packedBins = binPacking(cuts, boardWidth, boardHeight);
 
-return (
-  <div className={classNames('BoardDisplay', 'board-display')}>
-    <div>
-      <img
-        src={selectedImage}
-        className={classNames('selected-image')}
-      />
-      <div className='block-row'>
-      {blocks.map((block, blockIndex) => (
+    const newBlocks = packedBins.reduce((acc, bin) => [...acc, ...bin.blocks], []);
+
+    setBlocks(newBlocks);
+  }, [cuts, boardWidth, boardHeight]);
+
+  return (
+    <div className={classNames('BoardDisplay', 'board-display')}>
+      <div>
+        <img
+          src={selectedImage}
+          className={classNames('selected-image')}
+        />
+        <div className="block-row">
+        {blocks.map((block, blockIndex) => (
         <div
           key={blockIndex}
-            className={classNames('Block', 'block')}
-            style={{
-              width: block.width + 'px',
-              height: block.height + 'px',
-              left: block.left + 'px',
-              top: block.top + 'px',
-            }}
-          >
-         </div>
+          className={classNames('Block', 'block')}
+          style={{
+            width: block.width + 'px',
+            height: block.height + 'px',
+            left: block.left + 'px',
+            top: block.top + 'px',
+        }}
+      ></div> 
       ))}
+        </div>
       </div>
     </div>
-  </div>
- );
+  );
 }
 
 export default BoardDisplay;
--- src/components/InputForm.jsx
+++ src/components/InputForm.jsx
@@ -6,7 +6,7 @@ import greenImage from '../assets/images/green.jpg';
 import brownImage from '../assets/images/brown.jpg';
 import paintedImage from '../assets/images/painted.jpg';
 
-function InputForm({ addCut, cuts, setCuts, onImageSelect, }) {
+function InputForm({ addCut, cuts, setCuts, onImageSelect }) {
   const [height, setHeight] = useState('');
   const [width, setWidth] = useState('');
   const [quantity, setQuantity] = useState('');
@@ -19,123 +19,131 @@ function InputForm({ addCut, cuts, setCuts, onImageSelect, }) {
 
   const maxAllowedCuts = 10;
 
-  
-const addCutHandler = useCallback(() => {
-  if (height && width && quantity) {
-    if (inputData.length < maxAllowedCuts) {
-      const newCut = {
-        id: Date.now(),
-        width: parseFloat(width),
-        height: parseFloat(height),
-        quantity: parseInt(quantity),
-      };
-    
-      addCut(newCut);
-      setInputCount(inputCount + 1);
-      setInputData([...inputData, newCut]);
-  
-      setHeight('');
-      setWidth('');
-      setQuantity('');
-    } else {
-      alert(`You can only add up to ${maxAllowedCuts} cuts.`);
+  const addBlockHandler = useCallback(() => {
+    if (height && width && quantity) {
+      if (inputData.length < maxAllowedCuts) {
+        const newCut = {
+          id: Date.now(),
+          width: parseFloat(width),
+          height: parseFloat(height),
+          quantity: parseInt(quantity),
+        };
+
+        addCut(newCut);
+        setInputCount(inputCount + 1);
+        setInputData([...inputData, newCut]);
+
+        setHeight('');
+        setWidth('');
+        setQuantity('');
+      } else {
+        alert(`You can only add up to ${maxAllowedCuts} cuts.`);
+      }
     }
-  }
-}, [addCut, height, width, inputCount, inputData, quantity])
+  }, [addCut, height, width, inputCount, inputData, quantity]);
 
+  const editInput = (index) => {
+    const updatedCuts = [...cuts];
 
-const editInput = (index) => {
-  const updatedCuts = [...cuts];
+    if (index >= 0 && index < updatedCuts.length) {
+      const editedCut = updatedCuts[index];
 
-  if (index >= 0 && index < updatedCuts.length) {
-    const editedCut = updatedCuts[index];
-    
-    const newWidth = prompt("Enter new width:", editedCut.width);
-    const newHeight = prompt("Enter new height:", editedCut.height);
-    const newQuantity = prompt("Enter new quantity:", editedCut.quantity);
+      const newWidth = prompt("Enter new width:", editedCut.width);
+      const newHeight = prompt("Enter new height:", editedCut.height);
+      const newQuantity = prompt("Enter new quantity:", editedCut.quantity);
 
-    if (newWidth !== null && newHeight !== null && newQuantity !== null) {
-      updatedCuts[index] = {
-        width: parseFloat(newWidth),
-        height: parseFloat(newHeight),
-        quantity: parseInt(newQuantity),
-      };
+      if (newWidth !== null && newHeight !== null && newQuantity !== null) {
+        updatedCuts[index] = {
+          width: parseFloat(newWidth),
+          height: parseFloat(newHeight),
+          quantity: parseInt(newQuantity),
+        };
+      }
     }
-  }
 
-  setCuts(updatedCuts);
-  setEditIndex(-1);
-};
-  
+    setCuts(updatedCuts);
+    setEditIndex(-1);
+  };
 
-const deleteInput = (index) => {
-  const cutId = inputData[index].id;
-  const updatedInputData = inputData.filter((data) => data.id !== cutId);
-  setInputData(updatedInputData);
-  const updatedCuts = [...cuts];
-  updatedCuts.splice(index, 1);
-  setCuts(updatedCuts);
-};
-const onImageClick = (image) => {
-  setSelectedImage(image);
-  onImageSelect(image);
-};
+  const deleteInput = (index) => {
+    const cutId = inputData[index].id;
+    const updatedInputData = inputData.filter((data) => data.id !== cutId);
+    setInputData(updatedInputData);
+    const updatedCuts = [...cuts];
+    updatedCuts.splice(index, 1);
+    setCuts(updatedCuts);
+  };
 
-return (
-  <div className="head-input">
-    <div className={classNames('InputForm', 'input-form-container')}>
-      <div className="image-selection">
-        {images.map((image, index) => (
-          <img
-            key={index}
-            src={image}
-            alt={`image ${index + 1}`}
-            onClick={() => onImageSelect(image)}
-            className={classNames('image', { 'selectedImage': image === selectedImage })}
-          />
-        ))}
-      </div>
-      <div className={classNames('input-group')}>
-        <label className={classNames('label')}>Width:</label>
-        <input
-          type="number"
-          className={classNames('input')}
-          value={width}
-          onChange={(e) => setWidth(e.target.value)}
-        />
-      </div>
-      <div className={classNames('input-group')}>
-        <label className={classNames('label')}>Height:</label>
-        <input
-          type="number"
-          className={classNames('input')}
-          value={height}
-          onChange={(e) => setHeight(e.target.value)}
-        />
-      </div>
-      <div className={classNames('input-group')}>
-        <label className={classNames('label')}>Quantity:</label>
-        <input
-          type="number"
-          className={classNames('input')}
-          value={quantity}
-          onChange={(e) => setQuantity(e.target.value)}
-        />
-      </div>
-      <button className={classNames('button')} onClick={addCutHandler}>
-        Add Cut
-      </button>
-      <div>
-        {inputData.map((data, index) => (
-          <div key={index}>
-            <button className='edit-btn' onClick={() => editInput(index)}>Edit</button>
-            <button className='delete-btn' onClick={() => deleteInput(index)}>Delete</button>
+  const handleKeyPress = (event) => {
+    if (event.key === 'Enter') {
+      event.preventDefault();
+      addBlockHandler();
+    }
+  };
+
+  return (
+    <div className="head-input">
+      <div className={classNames('InputForm', 'input-form-container')}>
+        <div className="image-selection">
+          {images.map((image, index) => (
+            <img
+              key={index}
+              src={image}
+              alt={`image ${index + 1}`}
+              onClick={() => onImageSelect(image)}
+              className={classNames('image', { 'selectedImage': image === selectedImage })}
+            />
+          ))}
+        </div>
+        <form onSubmit={(e) => {e.preventDefault(); addBlockHandler();}}>
+          <div className={classNames('input-group')}>
+            <label className={classNames('label')}>Width:</label>
+            <input
+              type="number"
+              className={classNames('input')}
+              value={width}
+              onChange={(e) => setWidth(e.target.value)}
+              onKeyPress={handleKeyPress}
+            />
           </div>
-        ))}
+          <div className={classNames('input-group')}>
+            <label className={classNames('label')}>Height:</label>
+            <input
+              type="number"
+              className={classNames('input')}
+              value={height}
+              onChange={(e) => setHeight(e.target.value)}
+              onKeyPress={handleKeyPress}
+            />
+          </div>
+          <div className={classNames('input-group')}>
+            <label className={classNames('label')}>Quantity:</label>
+            <input
+              type="number"
+              className={classNames('input')}
+              value={quantity}
+              onChange={(e) => setQuantity(e.target.value)}
+              onKeyPress={handleKeyPress}
+            />
+          </div>
+          <button className={classNames('button')} type="submit">
+            Add Blocks
+          </button>
+        </form>
+        <div>
+          {inputData.map((data, index) => (
+            <div key={index}>
+              <label htmlFor="">
+                {data.width}px, {data.height}px
+              </label>
+              <button className='edit-btn' onClick={() => editInput(index)}>Edit</button>
+              <button className='delete-btn' onClick={() => deleteInput(index)}>Delete</button>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
 }
 
 export default InputForm;
